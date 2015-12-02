301bbb3235f253a6a37a6cd5b30459c2b3676c09
cwcrane <ccrane@excelsiorintegrated.com>
2015-12-01 22:29:29 -0500

    Adds search/query option to pCtrl index method. -cwc

--- controllers/product.js
+++ controllers/product.js
@@ -6,11 +6,19 @@ mongoose.Promise = global.Promise;
 module.exports = mongoose.connection;
 
 var index = function (req, res, next) {
-  Product.find({}).exec().then(function (products) {
-    res.json(products);
-  }).catch(function(error){
-    next(error);
-  });
+  if (req.query.q){
+    Product.find({"name": { "$regex": req.query.q, "$options": "i" }}).exec().then(function (products) {
+      res.json(products);
+    }).catch(function(error){
+      next(error);
+    });
+  }else{
+    Product.find({}).exec().then(function (products) {
+      res.json(products);
+    }).catch(function(error){
+      next(error);
+    });
+  }
 };
 
 var show = function (req, res, next) {
@@ -21,6 +29,15 @@ var show = function (req, res, next) {
     next(error);
   });
 };
+
+// var search = function (req, res, next) {
+//   console.log("made it to search controller");
+//   Product.find({"name": req.params.id}).exec().then(function (products) {
+//     res.json(products);
+//   }).catch(function(error){
+//     next(error);
+//   });
+// };
 
 var create = function (req, res, next) {
 

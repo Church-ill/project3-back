6fa5a29c0347a3623ac3a6de68df3de7823ca768
Holden Mills <mholdenmills@gmail.com>
2015-12-02 17:32:26 -0500

    Adds user to login message

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,7 @@
 var express = require('express');
 var router = express.Router();
-
+var authCtrl = require('../controllers/auth');
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.get('/', authCtrl.root.get);
 
 module.exports = router;
